4d227addd36e1ae0cac49b31ec4c426f6714d5b7
SyedSabur <77959421+SyedSabur@users.noreply.github.com>
2022-03-29 12:51:20 -0700

    Add files via upload

---
+++ altres.js
@@ -0,0 +1,77 @@
+// ----------------------------------------  chapter1  ----------------------------------------
+
+// alert("javascript Alerts");
+
+// -----------------Q2--------------------
+
+// var valid_password ="Error! please enter valid password."
+// alert(valid_password);
+
+// -----------------Q3--------------------
+
+// var Welcome_to_js = "Welcome to js land... \n Happy coding!"
+// alert(Welcome_to_js);
+
+// -----------------Q4--------------------
+
+// alert(Welcome_to_js);
+
+// -----------------Q5--------------------
+
+// console.log("Hello... i can run jsthrought my web browser's console" );
+
+
+// ----------------------------------------  chapter2  ----------------------------------------
+
+
+// -----------------Q1--------------------
+
+// var username ="Syed"
+
+// -----------------Q2--------------------
+
+// var myname ="Sabur"
+
+// -----------------Q3--------------------
+
+// var message ="\nHello world"
+
+// -----------------Q4--------------------
+
+// alert(username + myname + message)
+
+// -----------------Q5--------------------
+
+// var pizza ="pizza"
+// var pizz ="\npizz"
+// var piz ="\npiz"
+// var pi ="\npi"
+// var p ="\np"
+
+// alert(pizza + pizz + piz + pi + p)
+
+// -----------------Q6--------------------
+
+// var email =" sdsabur23@gmail.com"
+// alert("My email address is" + email)
+
+// -----------------Q7--------------------
+
+// var A_smarter_way_to_learn_JavaScript ="I am trying to learn from the book. A smarter way to learn JavaScript"
+// alert(A_smarter_way_to_learn_JavaScript)
+
+// -----------------Q8--------------------
+
+document.getElementById("para").innerHTML = "Yah! I can write HTML content throught javascript";
+
+// -----------------Q9--------------------
+
+// alert("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬")
+
+
+// ----------------------------------------  chapter2  ----------------------------------------
+
+// -----------------Q1--------------------
+
+// var age =" 21"
+// alert("I am " + age + " years old" )
